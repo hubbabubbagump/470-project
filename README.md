5ab250486a5aef5a3bdec09a7b190037240ac0c3
Ethan Jung <ethanj@sfu.ca>
2018-10-19 16:03:37 -0700

    Push to initialize the database

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 A web application designed to facilitate the trading of school materials such as textbooks.
 
 To  run:
-    1. Install PHP on your system (http://php.net/downloads.php)
-    2. If on Windows, add the PHP installation directory to PATH
-    3. Run 'php -S localhost:<port>' to run the server
+    1. Clone/download the repo.
+    2. Add the 'php' folder to PATH.
+    3. Download MongoDB Community Edition (https://docs.mongodb.com/manual/administration/install-community/)
+    4. Add the 'bin' folder inside the MongoDB install location to PATH.
+    5. Run the database server locally by entering the command 'mongod --dbpath="\data\db"'.
+    6. Initialize the web application by running 'setup.php' within the 'setup' folder.
+    7. Run the web application by navigating to the 'webroot' folder and running 'php -S localhost:\<port\>'
+
+Adding scripts to setup the web application (e.g. adding database scripts):
+- Add the .php script to the 'setup' folder and include the file in 'setup.php'.
+- The script can be loaded by running 'php \<scriptname\>' without having to reinitialize the web application. Otherwise, run 'setup.php' to load the script.
---
+++ setup/database.php
@@ -0,0 +1,75 @@
+<?php
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    function printCollection ($collection) {
+        $cursor = $collection->find();
+        foreach ( $cursor as $id => $value ) {
+            echo "$id: ";
+            print_r(json_encode(json_decode(BSONtoJSON($value)), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) . "\n");
+        }
+    }
+
+    function BSONtoJSON ($bson) {
+        return MongoDB\BSON\toJSON(MongoDB\BSON\fromPHP($bson));
+    }
+
+    function insertUser ($collection, $name, $email, $password, $isAdmin) {
+        $id = uniqid();
+        $result = $collection->insertOne([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            '_id' => $id,
+            'isAdmin' => $isAdmin
+        ]);
+        echo "[" . $collection->getCollectionName() . "] Inserted new sales item with id: " . $result->getInsertedId() . "\n"; 
+    }
+
+    function addAdminUser ($collection) {
+        insertUser($collection, 'Admin', 'admin@admin.ca', 'password', True);
+    }
+
+    function addItem ($collection, $title, $sellerID, $faculty, $courseNum, $desc) {
+        $date = new DateTime();
+        $timestamp = $date->getTimestamp();
+        $id = uniqid();
+       $result = $collection->insertOne([
+            'title' => $title,
+            'faculty' => $faculty,
+            'courseNum' => $courseNum,
+            'desc' => $desc,
+            'datePosted' => $timestamp,
+            '_id' => $id,
+            'seller' => $sellerID
+        ]);
+        echo "[" . $collection->getCollectionName() . "] Inserted new sales item with id: " . $result->getInsertedId() . "\n"; 
+    }
+
+    //https://www.mongodb.com/download-center?jmp=docs#production
+    //https://docs.mongodb.com/php-library/v1.2/reference/class/MongoDBClient/
+    $mongo = new MongoDB\Client("mongodb://localhost:27017");
+    echo "Connected to MongoDB Client on port 27017\n";
+
+    try {
+        $local = $mongo->local;
+        echo "Local database selected\n";
+    }
+    catch (MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
+        echo $e;
+    }
+    
+    //Delete database collection for fresh startup
+    $mongo->dropDatabase("local");
+
+    //Create collection for users
+    $local->createCollection("users");
+    $users = $local->users;
+    echo "[" . $local->getDatabaseName() . "] Created new collection: " . $users->getCollectionName() . "\n";
+    addAdminUser($users);
+
+    //Create collection for items that people are selling
+    $local->createCollection("saleItems");
+    $saleItems = $local->saleItems;
+    echo "[" . $local->getDatabaseName() . "] Created new collection: " . $saleItems->getCollectionName() . "\n";
+    addItem($saleItems, "An example textbook", "5345ds3iaf", "CMPT", 470, "Slightly used textbook in good condition");
+?>
