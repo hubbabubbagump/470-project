f4e2d2bef5c3e37727cc51f03813a0425f6b3033
avneetk <avneetk@sfu.ca>
2018-10-30 15:48:26 -0700

    pulled and merged

---
+++ application/views/search.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<script src="/js/jquery-3.3.1.min.js"></script>
+	<script type='text/javascript' src="/js/header.js"></script>
+	<script type='text/javascript' src="/js/search.js"></script>
+	<link rel="stylesheet" href="/css/header.css">
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to 470 Project!</h1>
+
+	<div id="body">
+		<p id="text"> Hello World </p>
+	</div>
+
+	<button type='button' id='test' onClick='getItems()'>Click me</button>
+	
+</div>
+
+</body>
+</html>
