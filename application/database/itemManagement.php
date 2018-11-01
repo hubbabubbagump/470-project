0ef53ccf0434e16055c5514822479f5f66eee835
avneetk <avneetk@sfu.ca>
2018-10-31 17:14:55 -0700

    changed include utilities from DATABASE to __DIR__

--- application/database/itemManagement.php
+++ application/database/itemManagement.php
@@ -1,6 +1,6 @@
 <?php
     
-    require_once DATABASE . "utilities.php";
+    include_once __DIR__ . "../utilities.php";
 
     function insertItem ($collection, $title, $sellerID, $faculty, $courseNum, $desc, $price): string {
 
