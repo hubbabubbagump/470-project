a89e3f0033767dfb1dad4e3c675763298b62b217
vqyuan <victor_yuan_2@sfu.ca>
2018-11-19 17:30:46 -0800

    Replace messageTable.php

--- setup/messageTable.php
+++ setup/messageTable.php
@@ -1,27 +1,11 @@
 <?php
+    include_once __DIR__ . "/database.php";
 
-    include_once __DIR__ . "/../application/database/messageManagement.php";
-    include_once __DIR__ . "/../application/database/settings.php";
+    foreach(glob(__DIR__ . "/*.php") as $file) {
+        if (strpos($file, "database.php") !== false) {
+            continue;
+        }
 
-    $mongo = new MongoDB\Client(getDBAddr());
-
-    try {
-        $local = $mongo->local;
+        include_once $file;
     }
-    catch (MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
-        echo $e;
-    }
-
-    //Collection for messages sent between users
-    $local->createCollection("messages");
-    $messages = $local->messages;
-    echo "[" . $local->getDatabaseName() . "] Created new collection: " . $saleItems->getCollectionName() . "\n";
-
-    //Sample message
-    sendMessage($messages, "StudentA", "StudentB", "[Generic Greeting]");
-    sendMessage($messages, "StudentA", "StudentB", "[Generic Follow-up]");
-    sendMessage($messages, "StudentA", "StudentB", "[Agitated Follow-up]");
-
-    //Get messages sent to Student B
-    retrieveMessage($messages, "StudentB");
 ?>
