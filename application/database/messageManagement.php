02560c99f92729a1f13d49edbfb60dd294e445c0
vqyuan <victor_yuan_2@sfu.ca>
2018-11-19 17:22:00 -0800

    Replace messageManagement.php

--- application/database/messageManagement.php
+++ application/database/messageManagement.php
@@ -2,27 +2,112 @@
     
     include_once __DIR__ . "../utilities.php";
 
-    function sendMessage ($collection, $senderID, $recipientID, $message): string {
+    function sendMessage ($collection, $senderEmail, $recipientEmail, $message): string {
 
         $date = new DateTime();
         $timestamp = $date->getTimestamp();
         $id = uniqid();
         $result = $collection->insertOne([
-            'senderID' => $senderID,
-            'recipientID' => $recipientID,
+            'senderEmail' => $senderEmail,
+            'recipientEmail' => $recipientEmail,
             'message' => $message,
             'dateSent' => $timestamp,
         ]);
-        //Should echo
-        echo "[" . $collection->getCollectionName() . "] Sent new message from: {" . $senderID . "} to: {" . $recipientID . "}\n"; 
+        
+        echo "[" . $collection->getCollectionName() . "] Sent new message from: {" . $senderEmail . "} to: {" . $recipientEmail . "}\n"; 
 
         return $id;
     }
 
-    function retrieveMessage($collection, $recipientID) {
-        $cursor = $collection->find(['recipientID' => $recipientID]);
-        foreach ($cursor as $doc) {
-            var_dump($doc);
+    function retrieveMessage($collection, $senderEmail, $recipientEmail) {
+        $cursor = $collection->find(array(
+            '$or' => array(
+                array(
+                    '$and' => array(
+                        array(
+                            'senderEmail' => $senderEmail
+                        ),
+                        array(
+                            'recipientEmail' => $recipientEmail
+                        )
+                    )
+                ),
+                array(
+                    '$and' => array(
+                        array(
+                            'senderEmail' => $recipientEmail
+                        ),
+                        array(
+                            'recipientEmail' => $senderEmail
+                        )
+                    )
+                ),
+            )
+        ));
+
+        $conversation = array();
+
+        $cursor = $cursor->toArray();
+
+        foreach ($cursor as $message) {
+            array_push($conversation, $message);
         }
+
+        return json_encode(array('conversation' => $conversation));
+
+        //echo json_encode(iterator_to_array($cursor, false));
+        /*foreach ($cursor as $doc) {
+            var_dump($doc);
+        }*/
+    }
+
+    function getParticipants($collection, $senderEmail) {
+        $query1 = array('senderEmail' => $senderEmail);
+        //$query2 = array('recipientEmail' => $senderEmail);
+        //Didn't find a way to combine two queries, so I made another
+
+        $projection1 = array('projection' => array('recipientEmail' => true));
+        //$projection2 = array('projection' => array('senderEmail' => true));
+
+        $cursor1 = $collection->find($query1, $projection1);
+        /*foreach ($cursor1 as $doc1) {
+            print_r($doc1);
+        }*/
+
+        /*$cursor2 = $collection->find($query2, $projection2);
+        foreach ($cursor2 as $doc2) {
+            print_r($doc2);
+        }*/
+
+        $participants = array();
+        $cursor1 = $cursor1->toArray();
+
+        foreach ($cursor1 as $contact) {
+            array_push($participants, $contact);
+        };
+
+        return json_encode(array('participants' => $participants));
+
+        /*$cursor2 = $cursor2->toArray();
+
+        $length1 = count($cursor1);
+        $length2 = count($cursor2);
+
+        $i1 = 0;
+        $i2 = 0;
+
+        foreach ($cursor1 as $contact) {
+            if ($i1 < length1) {
+                array_push($participants, $contact);
+                $i1 += 1;
+            }
+        };
+
+        foreach ($cursor2 as $contact) {
+            if ($i2 < length2) {
+                array_push($participants, $contact);
+                $i2 += 1;
+            }
+        };*/
     }
 ?>
