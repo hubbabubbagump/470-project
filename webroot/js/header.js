60852427b863461098399829688c85d529a070df
vqyuan <victor_yuan_2@sfu.ca>
2018-11-20 13:25:09 -0800

    Replace header.js

--- webroot/js/header.js
+++ webroot/js/header.js
@@ -1,22 +1,25 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<body>
-    <div class="header">
-        <div class="labelContainer"><label onclick="openHome()">470 Project</label></div>  
-        <div class="loginregister">
-        <?php if ($showLogin): ?>
-            <div class="buttonContainer" onclick="openLogin()"><p class="navigate login" id="login">Login</p></div>
-            <div class="buttonContainer" onclick="openRegister()"><p class="navigate register"id="register">Register</p></div>
-        <?php else: ?>
-            <?php if ($showPostItem): ?>
-                <div class="buttonContainer" onclick="openPostItem()"><button class="navigate" id="addItem">Post New Item</button></div>
-            <?php endif;?>
+'use strict'
 
-            <div class="buttonContainer" onclick="openMessages()"><button class="navigate" id="logout">Messages</button></div>
+function openLogin() {
+    window.location.href = "/login";
+}
 
-            <div class="buttonContainer" onclick="openLogout()"><button class="navigate" id="logout">Logout</button></div>
-        <?php endif; ?>
-        </div>
-    </div>
-</body>
+function openRegister() {
+    window.location.href = "/register";
+}
+
+function openHome() {
+    window.location.href = "/";
+}
+
+function openLogout() {
+    window.location.href = "/logout";
+}
+
+function openPostItem() {
+	window.location.href = "/Item";
+}
+
+function openMessages() {
+    window.location.href = "/Message"
+}
