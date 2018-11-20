d5a6f80c7890595ea3a307cf7504fe7729792f2e
vqyuan <victor_yuan_2@sfu.ca>
2018-11-19 18:16:36 -0800

    Replace messageManagement.php
    Get has been fixed

--- application/database/messageManagement.php
+++ application/database/messageManagement.php
@@ -63,51 +63,30 @@
 
     function getParticipants($collection, $senderEmail) {
         $query1 = array('senderEmail' => $senderEmail);
-        //$query2 = array('recipientEmail' => $senderEmail);
+        $query2 = array('recipientEmail' => $senderEmail);
         //Didn't find a way to combine two queries, so I made another
 
         $projection1 = array('projection' => array('recipientEmail' => true));
-        //$projection2 = array('projection' => array('senderEmail' => true));
+        $projection2 = array('projection' => array('senderEmail' => true));
 
         $cursor1 = $collection->find($query1, $projection1);
+        $cursor2 = $collection->find($query2, $projection2);
         /*foreach ($cursor1 as $doc1) {
             print_r($doc1);
         }*/
 
-        /*$cursor2 = $collection->find($query2, $projection2);
-        foreach ($cursor2 as $doc2) {
-            print_r($doc2);
-        }*/
-
         $participants = array();
-        $cursor1 = $cursor1->toArray();
 
+        $cursor1 = $cursor1->toArray(); 
         foreach ($cursor1 as $contact) {
             array_push($participants, $contact);
         };
 
-        return json_encode(array('participants' => $participants));
-
-        /*$cursor2 = $cursor2->toArray();
-
-        $length1 = count($cursor1);
-        $length2 = count($cursor2);
-
-        $i1 = 0;
-        $i2 = 0;
-
-        foreach ($cursor1 as $contact) {
-            if ($i1 < length1) {
-                array_push($participants, $contact);
-                $i1 += 1;
-            }
+        $cursor2 = $cursor2->toArray();
+        foreach ($cursor2 as $contact) {
+            array_push($participants, $contact);
         };
 
-        foreach ($cursor2 as $contact) {
-            if ($i2 < length2) {
-                array_push($participants, $contact);
-                $i2 += 1;
-            }
-        };*/
+        return json_encode(array('participants' => $participants));
     }
 ?>
